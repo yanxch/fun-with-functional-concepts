8260b83f5c5dd947f0c99c00929803b0921c0260
Christian Janker <janker.christ@gmail.com>
2019-02-23 07:50:51 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'having-fun-with-functional-concepts';
+}
